b5aa0f1c61292605ecef74862bea77d1fd6446c0
Jiri Spilka <jirka.spilka@gmail.com>
2024-07-29 11:02:46 +0200

    Refactor Apify dataset loader

--- src/apify_haystack/apify_dataset.py
+++ src/apify_haystack/apify_dataset.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import os
-from abc import ABC, abstractmethod
 from typing import Callable
 
 from apify_client import ApifyClient
@@ -32,11 +31,20 @@ class ApifyDatasetLoader:
             dataset = loader.run()
     """
 
-    def __init__(self, dataset_id: str, dataset_mapping_function: Callable[[dict], Document]) -> None:
+    def __init__(
+        self,
+        dataset_id: str,
+        dataset_mapping_function: Callable[[dict], Document],
+    ) -> None:
+        """Initialize the Apify dataset loader.
 
+        Args:
+            dataset_id (str): The ID of the dataset on the Apify platform.
+            dataset_mapping_function (Callable): A function that takes a single (an Apify dataset item) and converts
+                it to an instance of the Document class.
+        """
         self.dataset_id = dataset_id
         self.dataset_mapping_function = dataset_mapping_function
-        self.client = ApifyClient()
 
         # Add a custom user-agent to the httpx client for attribution purposes
         self.client = ApifyClient()
@@ -44,45 +52,18 @@ class ApifyDatasetLoader:
             httpx_client.headers["user-agent"] += f"; {HAYSTACK_ATTRIBUTE_USER_AGENT}"
 
     @component.output_types(documents=list[Document])  # type: ignore[misc]
-    def run(self) -> list[Document]:
+    def run(
+        self,
+    ) -> list[Document]:
         """Load datasets produced by the `Apify Actors`."""
         dataset_items = self.client.dataset(self.dataset_id).list_items(clean=True).items
         return list(map(self.dataset_mapping_function, dataset_items))
 
 
-@component
-class ApifyDatasetBase(ABC):
-    """Base class for wrapper around Apify Actors.
+# @component
+# class ApifyKeyValueStoreLoader:
 
-    Perform call of Apify actors or tasks, and load datasets produced by them.
-    """
-
-    def __init__(
-        self,
-        actor_or_task_id: str,
-        dataset_mapping_function: Callable[[dict], Document],
-        apify_api_token: str | None = None,
-    ) -> None:
-        if not (apify_api_token := apify_api_token or os.getenv("APIFY_API_TOKEN")):
-            raise ValueError(
-                "Apify API token not found. Please provide Apify API token when initializing "
-                "ApifyDatasetCall, or set the environment variable APIFY_API_TOKEN."
-            )
-
-        self.actor_or_task_id = actor_or_task_id
-        self.dataset_mapping_function = dataset_mapping_function
-        self.client = ApifyClient(apify_api_token)
-
-        if httpx_client := getattr(self.client.http_client, "httpx_client", None):
-            httpx_client.headers["user-agent"] += f"; {HAYSTACK_ATTRIBUTE_USER_AGENT}"
-
-    @abstractmethod
-    def run(self) -> list[Document]:
-        """Run an Actor on the Apify platform and wait for results to be ready."""
-
-
-@component
-class ApifyDatasetFromActorCall(ApifyDatasetBase):
+class ApifyDatasetFromActorCall:
     """Get Apify dataset by calling Apify Actor.
 
     Perform call of Apify actor and load produced dataset.
@@ -97,31 +78,15 @@ class ApifyDatasetFromActorCall(ApifyDatasetBase):
         self,
         actor_id: str,
         dataset_mapping_function: Callable[[dict], Document],
-        apify_api_token: str | None = None,
-    ) -> None:
-        """Initialize the loader with an Apify actor ID and a mapping function.
-
-        Args:
-            actor_id (str): The ID or name of the actor on the Apify platform.
-            dataset_mapping_function (Callable): A function that takes a single (an Apify dataset item) and converts
-                it to an instance of the Document class.
-            apify_api_token (str, optional): The API token for the Apify platform. If not provided, it will be read
-                from the environment
-        """
-        super().__init__(actor_id, dataset_mapping_function, apify_api_token)
-
-    @component.output_types(documents=list[Document])  # type: ignore[misc]
-    def run(
-        self,
-        actor_id: str,
         run_input: dict,
-        dataset_mapping_function: Callable[[dict], Document],
         *,
         build: str | None = None,
         memory_mbytes: int | None = None,
         timeout_secs: int | None = None,
-    ) -> list[Document]:
-        """Run an Actor on the Apify platform and wait for results to be ready.
+        apify_api_token: str | None = None,
+    ) -> None:
+        """
+        Initialize the Apify Actor loader.
 
         Args:
             actor_id (str): The ID or name of the Actor on the Apify platform.
@@ -131,91 +96,47 @@ class ApifyDatasetFromActorCall(ApifyDatasetBase):
                 instance of the Document class.
             build (str, optional): Optionally specifies the actor build to run.
                 It can be either a build tag or build number.
-            memory_mbytes (int, optional): Optional memory limit for the run,
-                in megabytes.
+            memory_mbytes (int, optional): Optional memory limit for the run, in megabytes.
             timeout_secs (int, optional): Optional timeout for the run, in seconds.
-
-        Returns:
-            list[Document]: A list of documents produced by the actor.
+            apify_api_token (str, optional): The API token for the Apify platform.
+                If not provided, it will be read from the environment
         """
+        self.actor_id = actor_id
+        self.dataset_mapping_function = dataset_mapping_function
+        self.run_input = run_input
+        self.build = build
+        self.memory_mbytes = memory_mbytes
+        self.timeout_secs = timeout_secs
+
+        if not (apify_api_token := apify_api_token or os.getenv("APIFY_API_TOKEN")):
+            raise ValueError(
+                "Apify API token not found. Please provide Apify API token when initializing "
+                "ApifyDatasetCall, or set the environment variable APIFY_API_TOKEN."
+            )
+        # Add a custom user-agent to the httpx client for attribution purposes
+        self.client = ApifyClient(apify_api_token)
+
+        if httpx_client := getattr(self.client.http_client, "httpx_client", None):
+            httpx_client.headers["user-agent"] += f"; {HAYSTACK_ATTRIBUTE_USER_AGENT}"
+
+    @component.output_types(documents=list[Document])  # type: ignore[misc]
+    def run(self) -> list[Document]:
+        """Run an Actor on the Apify platform and wait for results to be ready."""
         if not (
-            actor_call := self.client.actor(actor_id).call(
-                run_input=run_input,
-                build=build,
-                memory_mbytes=memory_mbytes,
-                timeout_secs=timeout_secs,
+            actor_call := self.client.actor(self.actor_id).call(
+                run_input=self.run_input,
+                build=self.build,
+                memory_mbytes=self.memory_mbytes,
+                timeout_secs=self.timeout_secs,
             )
         ):
-            raise ValueError(f"No response found in the actor {actor_id} call")
+            raise ValueError(f"No response found in the actor {self.actor_id} call")
 
         if not (dataset_id := actor_call.get("defaultDatasetId")):
-            raise ValueError(f"No dataset found in the actor {actor_id} call response.")
+            raise ValueError(f"No dataset found in the actor {self.actor_id} call response.")
 
-        loader = ApifyDatasetLoader(
+        loader = ApifyDatasetLoader()
+        return loader.run(
             dataset_id=dataset_id,
-            dataset_mapping_function=dataset_mapping_function,
-        )
-        return loader.run()  # type: ignore[no-any-return]
-
-    @component
-    class ApifyDatasetFromTaskCall(ApifyDatasetBase):
-        """Get Apify dataset by calling Apify Task.
-
-        Perform call of Apify actor and load produced dataset.
-        For details, see https://docs.apify.com/platform/integrations/haystack
-
-        To use, you should have the ``apify-client`` python package installed,
-        and the environment variable ``APIFY_API_TOKEN`` set with your API key, or pass
-        `apify_api_token` as a named parameter to the constructor.
-        """
-
-        def __init__(
-            self,
-            actor_id: str,
-            dataset_mapping_function: Callable[[dict], Document],
-            apify_api_token: str | None = None,
-        ) -> None:
-            super().__init__(actor_id, dataset_mapping_function, apify_api_token)
-
-        @component.output_types(documents=list[Document])  # type: ignore[misc]
-        def run(
-            self,
-            task_id: str,
-            task_input: dict,
-            dataset_mapping_function: Callable[[dict], Document],
-            *,
-            build: str | None = None,
-            memory_mbytes: int | None = None,
-            timeout_secs: int | None = None,
-        ) -> list[Document]:
-            """Run a saved Actor task on Apify and wait for results to be ready.
-
-            Args:
-                task_id (str): The ID or name of the task on the Apify platform.
-                task_input (Dict): The input object of the task that you're trying to run.
-                    Overrides the task's saved input.
-                dataset_mapping_function (Callable): A function that takes a single
-                    dictionary (an Apify dataset item) and converts it to an
-                    instance of the Document class.
-                build (str, optional): Optionally specifies the actor build to run.
-                    It can be either a build tag or build number.
-                memory_mbytes (int, optional): Optional memory limit for the run,
-                    in megabytes.
-                timeout_secs (int, optional): Optional timeout for the run, in seconds.
-
-            Returns:
-                ApifyDatasetLoader: A loader that will fetch the records from the
-                    task run's default dataset.
-            """
-            if not (
-                task_call := self.client.task(task_id).call(
-                    task_input=task_input, build=build, memory_mbytes=memory_mbytes, timeout_secs=timeout_secs
-                )
-            ):
-                raise ValueError(f"No response found in the task {task_id} call")
-
-            if not (dataset_id := task_call.get("defaultDatasetId")):
-                raise ValueError(f"No dataset found in the task {task_id} call response.")
-
-            loader = ApifyDatasetLoader(dataset_id=dataset_id, dataset_mapping_function=dataset_mapping_function)
-            return loader.run()  # type: ignore[no-any-return]
+            dataset_mapping_function=self.dataset_mapping_function,
+        )  # type: ignore[no-any-return]
