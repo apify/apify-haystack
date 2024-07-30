89f288811e1f3f5f36e4ef6195074f05f4331df6
Jiri Spilka <jirka.spilka@gmail.com>
2024-07-30 14:22:32 +0200

    Fix typo in init.py

--- src/apify_haystack/__init__.py
+++ src/apify_haystack/__init__.py
@@ -2,6 +2,6 @@ from importlib import metadata
 
 __version__ = metadata.version("apify-haystack")
 
-from apify_haystack.apify_dataset import ApifyDatasetFromActorCall, ApifyDatasetLoader
+from apify_haystack.apify_dataset import ApifyDatasetFromActorCall, ApifyDatasetFromTaskCall, ApifyDatasetLoader
 
-__all__ = ["ApifyDatasetLoader", "ApifyDatasetFromActorCall", "ApifyDatasetFromActorCall"]
+__all__ = ["ApifyDatasetLoader", "ApifyDatasetFromActorCall", "ApifyDatasetFromTaskCall"]
