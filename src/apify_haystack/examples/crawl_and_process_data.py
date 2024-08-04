ba7e1468436019219e0110859ef7215752b3933d
Jiri Spilka <jirka.spilka@gmail.com>
2024-08-04 20:38:51 +0200

    Update

--- src/apify_haystack/examples/crawl_and_process_data.py
+++ src/apify_haystack/examples/crawl_and_process_data.py
@@ -2,7 +2,7 @@
 Crawl websites, scrape text content, and store it in the InMemoryDocumentStore.
 
 This script demonstrates how to extract content from a website using Apify's Website Content Crawler.
-The content is then cleaned, split into smaller chunks, embedded, and stored in the InMemoryDocumentStore.
+The content is split into smaller chunks, embedded, and stored in the InMemoryDocumentStore.
 
 After the pipeline is executed, the documents are retrieved from the document store
 using BM25 retrieval and vector similarity.
@@ -16,67 +16,45 @@ meta: {'url': 'https://haystack.deepset.ai/overview/quick-start',
  'split_id': 0, 'split_idx_start': 0, '_split_overlap': ......
 .....
 """
-
-import os
-
-from dotenv import load_dotenv
 from haystack import Document, Pipeline
 from haystack.components.embedders import OpenAIDocumentEmbedder, OpenAITextEmbedder
-from haystack.components.preprocessors import DocumentCleaner, DocumentSplitter
+from haystack.components.preprocessors import DocumentSplitter
 from haystack.components.writers import DocumentWriter
 from haystack.document_stores.in_memory import InMemoryDocumentStore
 from haystack.utils.auth import Secret
 
 from apify_haystack import ApifyDatasetFromActorCall
 
-load_dotenv()
-
-# Set APIFY_API_TOKEN here or use it from .env file
-apify_api_token = "" or os.getenv("APIFY_API_TOKEN")
-openai_api_key = "" or os.getenv("OPENAI_API_KEY")
-
-actor_id = "apify/website-content-crawler"
-run_input = {
-    "maxCrawlPages": 3,  # limit the number of pages to crawl
-    "startUrls": [{"url": "https://haystack.deepset.ai/"}],
-}
-
-
-def dataset_mapping_function(dataset_item: dict) -> Document:
-    return Document(content=dataset_item.get("text"), meta={"url": dataset_item.get("url")})
-
+apify_api_token = "YOUR-APIFY-API-TOKEN"
+openai_api_key = "YOUR-OPENAI-API-KEY"
 
 document_loader = ApifyDatasetFromActorCall(
-    actor_id=actor_id,
-    run_input=run_input,
-    dataset_mapping_function=dataset_mapping_function,
+    actor_id="apify/website-content-crawler",
+    run_input={
+        "maxCrawlPages": 3,  # limit the number of pages to crawl
+        "startUrls": [{"url": "https://haystack.deepset.ai/"}],
+    },
+    dataset_mapping_function=lambda item: Document(content=item["text"] or "", meta={"url": item["url"]}),
     apify_api_token=apify_api_token,
 )
 
 document_store = InMemoryDocumentStore()
 print(f"Initialized InMemoryDocumentStore with {document_store.count_documents()} documents")
 
-document_cleaner = DocumentCleaner()
 document_splitter = DocumentSplitter(split_by="word", split_length=150, split_overlap=50)
 document_embedder = OpenAIDocumentEmbedder(api_key=Secret.from_token(openai_api_key))
 document_writer = DocumentWriter(document_store)
 
 pipe = Pipeline()
 pipe.add_component("document_loader", document_loader)
-pipe.add_component("document_cleaner", document_cleaner)
 pipe.add_component("document_splitter", document_splitter)
 pipe.add_component("document_embedder", document_embedder)
 pipe.add_component("document_writer", document_writer)
 
-pipe.connect("document_loader", "document_cleaner")
-pipe.connect("document_cleaner", "document_splitter")
+pipe.connect("document_loader", "document_splitter")
 pipe.connect("document_splitter", "document_embedder")
 pipe.connect("document_embedder", "document_writer")
 
-print(
-    "Running pipeline the Apify document_loader -> document_cleaner -> document_splitter "
-    "-> document_embedder -> document_writer"
-)
 print("Crawling will take some time ...")
 print("You can visit https://console.apify.com/actors/runs to monitor the progress")
 
@@ -89,7 +67,8 @@ print("query='Haystack'")
 for doc in document_store.bm25_retrieval("Haystack", top_k=1):
     print(doc)
 
-print("Retrieving documents from the document store using vector similarity")
-print("query='What is Haystack'")
-for doc in document_store.embedding_retrieval(OpenAITextEmbedder().run("Haystack")["embedding"], top_k=1):
-    print(doc)
+print("Retrieving documents from the document store using vector similarity.")
+for doc in document_store.embedding_retrieval(
+    OpenAITextEmbedder(Secret.from_token(openai_api_key)).run("What is Haystack?")["embedding"], top_k=1
+):
+    print("The closest document to the query 'What is Haystack?':", doc)
